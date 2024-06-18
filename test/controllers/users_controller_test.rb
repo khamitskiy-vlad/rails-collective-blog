a06773735ec4dd4e1b0e66dd3828035b289c4751
Khamitskiy Vladislav <vladislaviktorovic@gmail.com>
2024-06-18 12:54:06 +0300

    removed unusible tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,8 +3,4 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test 'should get profile' do
-    get users_profile_url
-    assert_response :success
-  end
 end
