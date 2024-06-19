ecd2004ab116ced1c2a310954c74641343f8c98b
Khamitskiy Vladislav <vladislaviktorovic@gmail.com>
2024-06-19 22:00:47 +0300

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :posts
+  resources :categories
   devise_for :users
 
   root 'home#index'
+
+  get '/users/:id', to: 'users#show', as: 'user'
 end
