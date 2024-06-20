3decd79d719043dab16488de258d1a06eea17416
Khamitskiy Vladislav <vladislaviktorovic@gmail.com>
2024-06-20 23:40:22 +0300

    changed posts and categories routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :posts
-  resources :categories
+  resources :posts, except: :index
+  resources :categories, only: :show
   devise_for :users
 
   root 'home#index'
