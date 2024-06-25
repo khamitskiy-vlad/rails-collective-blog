36a54872a992263b82a9a5ff69178f8059bba4d1
Khamitskiy Vladislav <vladislaviktorovic@gmail.com>
2024-06-26 00:05:24 +0300

    disable verify_foreign_keys_for_fixtures

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,6 @@ module RailsProject64
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.active_record.verify_foreign_keys_for_fixtures = false
   end
 end
