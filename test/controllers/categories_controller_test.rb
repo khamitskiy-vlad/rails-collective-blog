f1ef5e16c501b7d074a28d4d9d98fd4191d32ee7
Khamitskiy Vladislav <vladislaviktorovic@gmail.com>
2024-06-26 22:02:20 +0300

    wrote test

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -3,7 +3,19 @@
 require 'test_helper'
 
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @category = categories(:one)
+    @post_one = posts(:one)
+    @post_two = posts(:two)
+    @post_three = posts(:three)
+  end
+
+  test "should get all category posts" do
+    get category_url(@category)
+    assert_response :success
+
+    assert_select 'a', @post_one.title
+    assert_select 'a', @post_two.title
+    assert_select 'a', { count: 0, text: @post_three.title }
+  end
 end
