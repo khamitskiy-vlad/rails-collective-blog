448c13958272ad343945402fc8e0642225afd06d
Khamitskiy Vladislav <vladislaviktorovic@gmail.com>
2024-07-02 23:54:59 +0300

    validations and relations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class Post < ApplicationRecord
-  belongs_to :creator, class_name: 'User'
   belongs_to :category
+  belongs_to :creator, class_name: 'User', inverse_of: :posts
+  has_many :comments, class_name: 'PostComment', dependent: :destroy
+
+  validates :title, presence: true, length: { minimum: 5, maximum: 150 }
+  validates :body, presence: true, length: { minimum: 150 }
 end
