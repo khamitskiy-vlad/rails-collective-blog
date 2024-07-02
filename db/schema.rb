0ccc296fb1730e23665601c5de8910dd65bcef6e
Khamitskiy Vladislav <vladislaviktorovic@gmail.com>
2024-07-03 01:05:05 +0300

    changed comment.body to comment.content

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_27_175800) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_02_215727) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,7 +22,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_27_175800) do
   end
 
   create_table "post_comments", force: :cascade do |t|
-    t.text "body", null: false
+    t.text "content", null: false
     t.bigint "post_id", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
