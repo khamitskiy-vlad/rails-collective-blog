7574ca6923fd1561d19ab1c32ba7b5a6e611ecaf
Khamitskiy Vladislav <vladislaviktorovic@gmail.com>
2024-07-02 23:57:48 +0300

    fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,21 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root 'home#index'
+
   shallow do
     resources :categories, param: :route, only: :show do
       resources :posts, except: %i[index show new create]
     end
   end
 
-  resources :posts, only: %i[new create show]
+  resources :posts, only: %i[new create show], shallow: true do
+    scope module: :posts do
+      resources :comments, only: %i[create destroy]
+    end
+  end
 
   devise_for :users
-
-  root 'home#index'
 
   get '/users/:id', to: 'users#show', as: 'user'
 end
