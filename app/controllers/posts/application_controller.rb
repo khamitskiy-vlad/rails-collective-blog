ce3afe3c84974a71e40be930dbec3f24ed2b5f41
Khamitskiy Vladislav <vladislaviktorovic@gmail.com>
2024-07-04 01:38:00 +0300

    added set post and authenticate_user! before action

--- app/controllers/posts/application_controller.rb
+++ app/controllers/posts/application_controller.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 class Posts::ApplicationController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_post
+
+  private
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
 end
