3ec1d8eb3d11a086931f2c39c05feccba37761a5
Khamitskiy Vladislav <vladislaviktorovic@gmail.com>
2024-07-05 15:06:22 +0300

    locale category

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module CategoriesHelper
+  def locale(category)
+    if I18n.locale == I18n.default_locale
+      category.name
+    else
+      category.route.capitalize
+    end
+  end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module PostsHelper
+  def locale_category_from(post)
+    if I18n.locale == I18n.default_locale
+      post.category.name
+    else
+      post.category.route.capitalize
+    end
+  end
 end
