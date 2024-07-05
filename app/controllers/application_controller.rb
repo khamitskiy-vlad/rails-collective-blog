a0028bdf3d3b6fac5ca8c5e5dcf8233519289422
Khamitskiy Vladislav <vladislaviktorovic@gmail.com>
2024-07-05 15:01:03 +0300

    switch_locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,17 @@
 
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  around_action :switch_locale
   before_action :set_categories
+
+  def switch_locale(&)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &)
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
 
   private
 
