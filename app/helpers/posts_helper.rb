a1beb862e838b9f79739bd3944796bdf61b810d3
Khamitskiy Vladislav <vladislaviktorovic@gmail.com>
2024-07-09 22:28:30 +0300

    removed localize categories

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,11 +1,4 @@
 # frozen_string_literal: true
 
 module PostsHelper
-  def locale_category_from(post)
-    if I18n.locale == I18n.default_locale
-      post.category.name
-    else
-      post.category.route.capitalize
-    end
-  end
 end
