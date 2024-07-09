a347f12a2ebd7fbb11548bd4eab40852f5dfbf60
Khamitskiy Vladislav <vladislaviktorovic@gmail.com>
2024-07-09 22:27:39 +0300

    set locale from header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,16 +5,18 @@ class ApplicationController < ActionController::Base
   around_action :switch_locale
   before_action :set_categories
 
-  def switch_locale(&)
-    locale = params[:locale] || I18n.default_locale
-    I18n.with_locale(locale, &)
-  end
-
-  def default_url_options
-    { locale: I18n.locale }
+  def switch_locale(&action)
+    logger.debug "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
+    locale = extract_locale_from_accept_language_header
+    logger.debug "* Locale set to '#{locale}'"
+    I18n.with_locale(locale, &action)
   end
 
   private
+
+  def extract_locale_from_accept_language_header
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+  end
 
   def set_categories
     @categories = Category.order(created_at: :asc)
