5abf91301c04d668c2242e91a8468eadda00f845
Khamitskiy Vladislav <vladislaviktorovic@gmail.com>
2024-07-15 00:49:00 +0300

    username presence validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'creator_id', inverse_of: :creator, dependent: :destroy
   has_many :comments, class_name: 'PostComment', dependent: :destroy
   has_many :likes, class_name: 'PostLike', dependent: :destroy
+
+  validates :username, presence: true
 end
