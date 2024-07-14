3b898b057f3128d19230864a002113ed6857ae3d
Khamitskiy Vladislav <vladislaviktorovic@gmail.com>
2024-07-14 19:28:39 +0300

    optimized db queries

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,7 +2,7 @@
 
 class CategoriesController < ApplicationController
   def show
-    @category = Category.find(params[:id])
-    @posts = @category.posts
+    @category = Category.find_by(id: params[:id])
+    @posts = @category.posts.includes(:likes, :comments, :creator)
   end
 end
