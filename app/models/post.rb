21c53b1392633f69373d6f6a744f171d6cd607e0
Khamitskiy Vladislav <vladislaviktorovic@gmail.com>
2024-07-18 01:16:37 +0300

    ransack validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,4 +8,12 @@ class Post < ApplicationRecord
 
   validates :title, presence: true, length: { minimum: 5, maximum: 150 }
   validates :body, presence: true, length: { minimum: 150 }
+
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[title body]
+  end
+
+  def self.ransackable_associations(_auth_object = nil)
+    %w[creator]
+  end
 end
