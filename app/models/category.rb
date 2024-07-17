cff531cf9152ac3593195b3283444477f78c40cb
Khamitskiy Vladislav <vladislaviktorovic@gmail.com>
2024-07-18 01:57:22 +0300

    uniqueness validations for categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,10 @@
 
 class Category < ApplicationRecord
   has_many :posts, dependent: :destroy
-
+  
+  validates :name, uniqueness: true
+  validates :route, uniqueness: true
+  
   def to_param
     route
   end
