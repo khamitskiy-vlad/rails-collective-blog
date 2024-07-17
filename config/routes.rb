196329684cda23add4baf3676e3297335e029110
Khamitskiy Vladislav <vladislaviktorovic@gmail.com>
2024-07-18 01:18:29 +0300

    locale and category routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,20 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root 'home#index'
+  scope '(:locale)', locale: /en|ru/ do
+    root 'home#index'
 
-  resources :categories, only: :show
+    resources :categories, param: :route, only: :show
 
-  resources :posts, only: %i[show new edit create update destroy] do
-    scope module: :posts do
-      resources :comments, only: %i[create destroy]
-      resources :likes, only: %i[create destroy]
+    resources :posts, only: %i[show new edit create update destroy] do
+      scope module: :posts do
+        resources :comments, only: %i[create destroy]
+        resources :likes, only: %i[create destroy]
+      end
     end
+
+    devise_for :users
+
+    resources :users, only: :show
   end
-
-  devise_for :users
-
-  resources :users, only: :show
 end
