ad410783083ee499c336e35916d16968db4baa62
Khamitskiy Vladislav <vladislaviktorovic@gmail.com>
2024-07-17 23:55:26 +0300

    added pagy::backend

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,20 @@
 # frozen_string_literal: true
 
 class CategoriesController < ApplicationController
+  include Pagy::Backend
+
   def show
-    @category = Category.find(params[:id])
-    @posts = @category.posts.includes(:creator)
+    @category = set_category
+    @pagy, @posts = pagy(set_posts)
+  end
+
+  private
+
+  def set_category
+    Category.find_by(route: params[:route])
+  end
+
+  def set_posts
+    @category.posts.includes(:creator)
   end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
+  include Pagy::Backend
+
   def index
-    @posts = Post.order(created_at: :desc).includes(:likes, :comments, :creator, :category)
+    @pagy, @posts = pagy(set_posts)
   end
+
+  private
+
+  def set_posts
+    Post.order(created_at: :desc).includes(:likes, :comments, :creator, :category)
+  end  
 end
